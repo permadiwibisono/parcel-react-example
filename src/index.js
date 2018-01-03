0a8bf3fee5a9cdebfde9466e7bc17e9d11896032
Permadi Wibisono <mbapewe@gmail.com>
2018-01-04 00:13:27 +0700

    Add script to bundle with parcel for development mode, add index.html and add dummy component

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {render} from 'react-dom';
+const App = ()=>{
+    return (
+        <h1>Hello World Parcel</h1>
+    )
+}
+
+render(
+    <App/>,
+    document.getElementById('root')
+);
